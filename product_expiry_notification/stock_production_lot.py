5444ec760da6cd6a6a94c33f375887096e49c46a
Ajeenckya Gadewar <ajinkya@bufferts.com>
2017-11-08 12:44:49 +0530

    Added logic for expiring products for 30,60,90 days

--- product_expiry_notification/stock_production_lot.py
+++ product_expiry_notification/stock_production_lot.py
@@ -59,10 +59,46 @@ class stock_production_lot(osv.osv):
                 res[obj_prodlot_id.id] = False
         return res
 
+    def _get_if_expiring_in_60_days(self, cr, uid, ids, field_name, arg, context=None):
+        """get if prodlots is expired based on life_date"""
+        if context is None: context = {}
+        res = {}
+        for obj_prodlot_id in self.browse(cr, uid, ids):
+            if obj_prodlot_id.life_date:
+                value = False
+                #check if prodlot is expired
+                if (datetime.strptime(obj_prodlot_id.life_date, "%Y-%m-%d %H:%M:%S") > (datetime.now()+timedelta(days=30))
+                    and datetime.strptime(obj_prodlot_id.life_date, "%Y-%m-%d %H:%M:%S") < (datetime.now()+timedelta(days=60))):
+                    value = True
+                res[obj_prodlot_id.id] = value
+            else:
+                res[obj_prodlot_id.id] = False
+        return res
+
+    def _get_if_expiring_in_90_days(self, cr, uid, ids, field_name, arg, context=None):
+        """get if prodlots is expired based on life_date"""
+        if context is None: context = {}
+        res = {}
+        for obj_prodlot_id in self.browse(cr, uid, ids):
+            if obj_prodlot_id.life_date:
+                value = False
+                #check if prodlot is expired
+                if (datetime.strptime(obj_prodlot_id.life_date, "%Y-%m-%d %H:%M:%S") > (datetime.now()+timedelta(days=60))
+                    and datetime.strptime(obj_prodlot_id.life_date, "%Y-%m-%d %H:%M:%S") < (datetime.now()+timedelta(days=90))):
+                    value = True
+                res[obj_prodlot_id.id] = value
+            else:
+                res[obj_prodlot_id.id] = False
+        return res
+
     _columns = {
         'expired': fields.function(_get_if_expired, method=True, type="boolean", string="Expired",
             store={'stock.production.lot': (lambda self, cr, uid, ids, c={}: ids, None, 20)}),
         'expire_30': fields.function(_get_if_expiring_in_30_days, method=True, type="boolean", string="Expiring in 30 days",
+            store={'stock.production.lot': (lambda self, cr, uid, ids, c={}: ids, None, 20)}),
+        'expire_30_60': fields.function(_get_if_expiring_in_60_days, method=True, type="boolean", string="Expiring in 30 to 60 days",
+            store={'stock.production.lot': (lambda self, cr, uid, ids, c={}: ids, None, 20)}),
+        'expire_60_90': fields.function(_get_if_expiring_in_90_days, method=True, type="boolean", string="Expiring in 60 to 90 days",
             store={'stock.production.lot': (lambda self, cr, uid, ids, c={}: ids, None, 20)})
     }
 
